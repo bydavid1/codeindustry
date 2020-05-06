8c98e734fa8ad975ced94346cb646e693e10bffb
byronjd <byronjimenez9911@gmail.com>
2020-05-05 22:22:22 -0600

    Info Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
-    
+    GitHubCalendar("#calendar", "byronjd", { responsive: true });
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "byronjd", selector: "#feed", limit: 5 });
