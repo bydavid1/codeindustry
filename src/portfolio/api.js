c585962d69f99a36d477fcecfe7fccc0b4ece9ff
bydavid1 <byronjimenez9911@gmail.com>
2021-06-26 11:06:04 -0600

    Api host from .env

--- src/portfolio/api.js
+++ src/portfolio/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.DB_HOST,
   headers: {
     "Content-type": "application/json"
   }
