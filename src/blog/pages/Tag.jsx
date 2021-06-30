d521242753050897bc2479859a0f522e67538325
bydavid1 <byronjimenez9911@gmail.com>
2021-06-30 17:00:35 -0600

    Tag page updated: Added 'loading' status

--- src/blog/pages/Tag.jsx
+++ src/blog/pages/Tag.jsx
@@ -11,9 +11,12 @@ class Tag extends Component {
 
     constructor(props) {
         super(props);
+
+        //data is reactive for component changes to work
         this.state = {
             tag: {},
             posts: {},
+            loading: true,
             status: 0
         }
     }
@@ -25,28 +28,40 @@ class Tag extends Component {
             this.setState({
                 posts: response.data.posts,
                 tag: response.data.tag,
-                status: 200
+                loading: false,
+                status: response.status
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false,
+                    status: error.response.status
+                })
             })
         })
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (this.props.match !== prevProps.match) {
+            //Show loader if tag is different
+            this.setState({
+                loading: true,
+            })
+
             let slug = this.props.match.params.slug
             api.get(`/tags/${slug}/posts`)
             .then(response => {
                 this.setState({
                     posts: response.data.posts,
                     tag: response.data.tag,
-                    status: 200
+                    loading: false,
+                    status: response.status
                 })
             })
             .catch(error => {
-                if (error.response.status == 404) {
-                    this.setState({
-                      status: 404
-                    })
-                }
+                this.setState({
+                    loading: false,
+                    status: error.response.status
+                })
             })
         }
     }
@@ -54,75 +69,81 @@ class Tag extends Component {
     
     
     render() {
-        if (this.state.status == 200) {
+        if (this.state.loading === true) {
             return (
-                <>
-                    <MetaTags>
-                        <title>{ this.state.tag.name }</title>
-                        <meta name="description" content={ this.state.tag.extract ? this.state.tag.extract : '' } /> 
-    
-                        {/* og meta */}
-                        <meta property="og:title" content={ this.state.tag.name }  />
-                        <meta property="og:description" content={ this.state.tag.extract ? this.state.tag.extract : '' } />
-                        <meta property="og:image" content={ this.state.tag.logo ? this.state.tag.logo.url : '' }  />
-                        <meta property="og:url" content={window.location.href}/>
-                        <meta property="og:type" content="website"/>
-                        {/* twitter meta */}
-                        <meta name="twitter:card" content="summary_large_image"/>
-                        <meta property="twitter:url" content={window.location.href}/>
-                        <meta name="twitter:title" content={ this.state.tag.name }/>
-                        <meta name="twitter:description" content={ this.state.tag.extract ? this.state.tag.extract : '' }/>
-                        <meta name="twitter:image" content={ this.state.tag.logo ? this.state.tag.logo.url : '' }/>
-                    </MetaTags>
-                    <div className="page-header">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-10">
-                                    <ul className="page-header-breadcrumb">
-                                        <li><Link to="/">Inicio</Link></li>
-                                        <li>Tag</li>
-                                        <li>{this.state.tag.name}</li>
-                                    </ul>
-                                    <h1>{this.state.tag.name}</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="section">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-8">
-                                    <div className="row">
-                                        {
-                                        this.state.posts.length > 0 ? (
-                                            this.state.posts.flatMap(post => 
-                                                    <div key={post._id} className="col-md-6">
-                                                        <Post style="thumb" title={post.title} cover={post.cover.url} date={post.date}
-                                                        category={post.category.title} postSlug={post.slug} categorySlug={post.category.slug}/>
-                                                    </div>
-                                                )
-                                            ) : (
-                                                <Loader/>   
-                                            )
-                                        }
-                                    </div>
-                                </div>
-                                <div className="col-md-4">
-                                    <div className="aside-widget text-center">
-                                        <a href="#" style={{display: 'inline-block', margin: 'auto'}}>
-                                            <img className="img-responsive" src={adPlaceholder} alt="" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            );
-        } else {
-            return (
-                <NotFound/>
+                <Loader/>
             )
+        } else {
+            if (this.state.status === 200) {
+                return (
+                    <>
+                        <MetaTags>
+                            <title>{ this.state.tag.name }</title>
+                            <meta name="description" content={ this.state.tag.extract ? this.state.tag.extract : '' } /> 
+        
+                            {/* og meta */}
+                            <meta property="og:title" content={ this.state.tag.name }  />
+                            <meta property="og:description" content={ this.state.tag.extract ? this.state.tag.extract : '' } />
+                            <meta property="og:image" content={ this.state.tag.logo ? this.state.tag.logo.url : '' }  />
+                            <meta property="og:url" content={window.location.href}/>
+                            <meta property="og:type" content="website"/>
+                            {/* twitter meta */}
+                            <meta name="twitter:card" content="summary_large_image"/>
+                            <meta property="twitter:url" content={window.location.href}/>
+                            <meta name="twitter:title" content={ this.state.tag.name }/>
+                            <meta name="twitter:description" content={ this.state.tag.extract ? this.state.tag.extract : '' }/>
+                            <meta name="twitter:image" content={ this.state.tag.logo ? this.state.tag.logo.url : '' }/>
+                        </MetaTags>
+                        <div className="page-header">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-md-10">
+                                        <ul className="page-header-breadcrumb">
+                                            <li><Link to="/">Inicio</Link></li>
+                                            <li>Tag</li>
+                                            <li>{this.state.tag.name}</li>
+                                        </ul>
+                                        <h1>{this.state.tag.name}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-md-8">
+                                        <div className="row">
+                                            {
+                                            this.state.posts.length > 0 ? (
+                                                this.state.posts.flatMap(post => 
+                                                        <div key={post._id} className="col-md-6">
+                                                            <Post style="thumb" title={post.title} cover={post.cover.url} date={post.date}
+                                                            category={post.category.title} postSlug={post.slug} categorySlug={post.category.slug}/>
+                                                        </div>
+                                                    )
+                                                ) : (
+                                                    <h3>No se encontraron articulos</h3>    
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <div className="aside-widget text-center">
+                                            <a href="#" style={{display: 'inline-block', margin: 'auto'}}>
+                                                <img className="img-responsive" src={adPlaceholder} alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                );
+            } else {
+                return (
+                    <NotFound/>
+                )
+            }
         }
     }
 }
