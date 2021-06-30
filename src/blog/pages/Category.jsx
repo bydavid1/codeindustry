7aa615b7f2e2c73b15b6b25cd21e79110f6dad3a
bydavid1 <byronjimenez9911@gmail.com>
2021-06-30 16:51:44 -0600

    Category page: now data is not reactive

--- src/blog/pages/Category.jsx
+++ src/blog/pages/Category.jsx
@@ -12,10 +12,12 @@ class Category extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            category: {},
-            posts: {},
+            loading: true,
             status: 0
         }
+
+        this.category = {}
+        this.posts = {}
     }
     
 
@@ -23,91 +25,98 @@ class Category extends Component {
         let slug = this.props.match.params.slug
         api.get('/posts/category/' + slug)
         .then(response => {
+            this.category = response.data.category
+            this.posts = response.data.posts
+
             this.setState({
-                category: response.data.category,
-                posts: response.data.posts,
-                status: 200
+                loading: false,
+                status: response.status
             })
         })
         .catch(error => {
-            if (error.response.status == 404) {
-                this.setState({
-                  status: 404
-                })
-            }
+            this.setState({
+                loading: false,
+                status: error.response.status
+            })
         })
 
     }
     
     render() {
-        if (this.state.status == 200) {
+        if (this.state.loading === true) {
             return (
-                <>
-                    <MetaTags>
-                        <title>{ this.state.category.title }</title>
-                        <meta name="description" content={ this.state.category.description } /> 
-    
-                        {/* og meta */}
-                        <meta property="og:title" content={ this.state.category.title }  />
-                        <meta property="og:description" content={ this.state.category.description } />
-                        <meta property="og:image" content={ this.state.category.cover ? this.state.category.cover.url : ''}  />
-                        <meta property="og:url" content={window.location.href}/>
-                        <meta property="og:type" content="website"/>
-                        {/* twitter meta */}
-                        <meta name="twitter:card" content="summary_large_image"/>
-                        <meta property="twitter:url" content={window.location.href}/>
-                        <meta name="twitter:title" content={ this.state.category.title }/>
-                        <meta name="twitter:description" content={ this.state.category.description }/>
-                        <meta name="twitter:image" content={ this.state.category.cover ? this.state.category.cover.url : ''}/>
-                    </MetaTags>
-                    <div className="page-header">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-10">
-                                    <ul className="page-header-breadcrumb">
-                                        <li><Link to="/">Inicio</Link></li>
-                                        <li>Category</li>
-                                    </ul>
-                                    <h1>{this.state.category.title}</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="section">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-8">
-                                    <div className="row">
-                                        {
-                                        this.state.posts.length > 0 ? (
-                                            this.state.posts.flatMap(post => 
-                                                <div key={post._id} className="col-md-6">
-                                                <Post style="thumb" title={post.title} cover={post.cover.url} date={post.date}
-                                                 category={this.state.category.title} postSlug={post.slug} categorySlug={this.state.category.slug}/>
-                                                </div>
-                                            )
-                                            ) : (
-                                                <Loader/>   
-                                            )
-                                        }
-                                    </div>
-                                </div>
-                                <div className="col-md-4">
-                                    <div className="aside-widget text-center">
-                                        <a href="#" style={{display: 'inline-block', margin: 'auto'}}>
-                                            <img className="img-responsive" src={adPlaceholder} alt="" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            );
-        } else {
-            return (
-                <NotFound/>
+                <Loader/>
             )
+        } else {
+            if (this.state.status === 200) {
+                return (
+                    <>
+                        <MetaTags>
+                            <title>{ this.category.title }</title>
+                            <meta name="description" content={ this.category.description } /> 
+        
+                            {/* og meta */}
+                            <meta property="og:title" content={ this.category.title }  />
+                            <meta property="og:description" content={ this.category.description } />
+                            <meta property="og:image" content={ this.category.cover ? this.category.cover.url : ''}  />
+                            <meta property="og:url" content={window.location.href}/>
+                            <meta property="og:type" content="website"/>
+                            {/* twitter meta */}
+                            <meta name="twitter:card" content="summary_large_image"/>
+                            <meta property="twitter:url" content={window.location.href}/>
+                            <meta name="twitter:title" content={ this.category.title }/>
+                            <meta name="twitter:description" content={ this.category.description }/>
+                            <meta name="twitter:image" content={ this.category.cover ? this.category.cover.url : ''}/>
+                        </MetaTags>
+                        <div className="page-header">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-md-10">
+                                        <ul className="page-header-breadcrumb">
+                                            <li><Link to="/">Inicio</Link></li>
+                                            <li>Category</li>
+                                        </ul>
+                                        <h1>{this.category.title}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-md-8">
+                                        <div className="row">
+                                            {
+                                            this.posts.length > 0 ? (
+                                                this.posts.flatMap(post => 
+                                                    <div key={post._id} className="col-md-6">
+                                                    <Post style="thumb" title={post.title} cover={post.cover.url} date={post.date}
+                                                     category={this.category.title} postSlug={post.slug} categorySlug={this.category.slug}/>
+                                                    </div>
+                                                )
+                                                ) : (
+                                                    <h3>No se encontraron articulos</h3>  
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <div className="aside-widget text-center">
+                                            <a href="#" style={{display: 'inline-block', margin: 'auto'}}>
+                                                <img className="img-responsive" src={adPlaceholder} alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                );
+            } else {
+                return (
+                    <NotFound/>
+                )
+            }
         }
     }
 }
