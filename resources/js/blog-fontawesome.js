8820b7a8eda15ad87407ef49561354db8975c9f3
bydavid1 <byronjimenez9911@gmail.com>
2021-07-03 15:42:24 -0600

    fontawesome in single file

--- resources/js/blog-fontawesome.js
+++
@@ -1,7 +0,0 @@
-import {
-    library
-} from '@fortawesome/fontawesome-svg-core';
-import { faFacebook, faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faBars, faEnvelope, faSearch, faShare, faTimes } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faSearch, faBars, faTimes, faFacebook, faTwitter, faGithub, faLinkedin, faEnvelope, faShare)
