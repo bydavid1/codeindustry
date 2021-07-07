1937db688910bc5c1da53af18334ad256b9cb689
bydavid1 <byronjimenez9911@gmail.com>
2021-07-06 18:51:12 -0600

    Index.js documentation

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const path = require('path')
 const cors = require('cors')
 const app = express();
 
+//Gettinf routes
 const routes = require('./api/routes')
 
-// const portfolio = require('./api/portfolio')
-// const index = require('./api/index')
-
+//Setup port 
 app.set('port', process.env.PORT || 3000)
 
+//Set assets path
 app.use(express.static(path.join(__dirname, './public/')))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 app.use('/', routes)
 
-// app.use('/portfolio', portfolio)
-// app.use('/', index)
 
+//Listen server (development only)
 app.listen(app.get('port'), () => console.log(`Server listen on http://127.0.0.1:${app.get('port')}`) )
