28bb2f50a4769b19947b3e81d070293268a6f319
bydavid1 <byronjimenez9911@gmail.com>
2021-07-06 18:48:28 -0600

    Routes merged in a single file

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path')
 const cors = require('cors')
-
 const app = express();
 
-const portfolio = require('./api/portfolio')
-const index = require('./api/index')
+const routes = require('./api/routes')
 
-app.set('port', process.env.PORT || 8080)
+// const portfolio = require('./api/portfolio')
+// const index = require('./api/index')
+
+app.set('port', process.env.PORT || 3000)
 
 app.use(express.static(path.join(__dirname, './public/')))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use('/', routes)
 
-
-app.use('/portfolio', portfolio)
-app.use('/', index)
-
+// app.use('/portfolio', portfolio)
+// app.use('/', index)
 
 app.listen(app.get('port'), () => console.log(`Server listen on http://127.0.0.1:${app.get('port')}`) )
